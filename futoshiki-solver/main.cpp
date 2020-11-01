88770cb01a9281abf86ec0f71500bc311a2d1578
MaxNoka <maxnoka@me.com>
2020-11-01 16:37:11 +0000

    Initial Commit

---
+++ futoshiki-solver/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  futoshiki-solver
+//
+//  Created by Maximilian Noka on 01/11/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
